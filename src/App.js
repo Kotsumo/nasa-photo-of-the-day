a963b4ac76bde67904c0aac25e658c4480d7e3b9
David <davidellis1020@gmail.com>
2021-02-18 14:30:01 -0800

    Pic is displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=JSc8agsqyVjYo2kt7mPxqJi1jYnpOLEDXgeiTYfC')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+    }, []);
+
+    console.log(data);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Pic of the Day {data.date}</h1>
+
+        <img src={data.hdurl} alt="pic of the day"/>
+
     </div>
   );
 }
