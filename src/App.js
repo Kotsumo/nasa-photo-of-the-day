7b95dd78a09bc0169ae5ac0a90caa1c86e0ba7aa
David <davidellis1020@gmail.com>
2021-02-18 17:30:57 -0800

    Added styling to title and img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from 'axios'
+import styled from "styled-components";
 
 function App() {
   const [data, setData] = useState([]);
@@ -15,12 +16,46 @@ function App() {
     console.log(data);
   return (
     <div className="App">
+      <StyledApp>
       <h1>Pic of the Day {data.date}</h1>
+      
+      <img src={data.url} alt="pic of the day"/>
 
-        <img src={data.hdurl} alt="pic of the day"/>
-
+      <p>
+        Here is the pic of the day.
+      </p>
+        
+        </StyledApp>
     </div>
   );
 }
+
+const StyledApp = styled.div`
+  
+  h1{
+    justify-content: center;
+    color: royalblue;
+    
+    &:hover{
+      color: teal;
+      transition: all 0.5s ease-in-out;
+    }
+
+    p {
+      display: flex;
+      padding: 10px;
+      margin: 0;
+      justify-content: right;
+      align-content: right;
+    }
+  }
+  
+  img {
+    justify-content: left;
+    display: flex;
+    padding: 1%;
+    margin: 10px;
+  }
+`
 
 export default App;
